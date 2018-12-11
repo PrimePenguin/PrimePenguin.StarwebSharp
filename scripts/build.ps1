be71a17d9e3e85a937c15ade44c1866b3b22ea07
Ajay Kumar <ajayak731@hotmail.com>
2018-12-11 18:18:25 +0530

    Updated package name

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -5,4 +5,4 @@ dotnet pack --no-build -c Release StarwebSharp/StarwebSharp.csproj;
 $nupkg = (Get-ChildItem StarwebSharp/bin/Release/*.nupkg)[0];
 
 # Push the nuget package to AppVeyor's artifact list.
-Push-AppveyorArtifact $nupkg.FullName -FileName $nupkg.Name -DeploymentName "StarwebSharp.nupkg";
+Push-AppveyorArtifact $nupkg.FullName -FileName $nupkg.Name -DeploymentName "PrimePenguin.StarwebSharp.nupkg";
