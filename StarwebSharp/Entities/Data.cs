60f92872327126710acfea10305d8733397d9a65
Ajay Kumar <ajayak731@hotmail.com>
2018-12-14 13:51:34 +0530

    Removed default initialization of customer address models

--- StarwebSharp/Entities/Data.cs
+++ StarwebSharp/Entities/Data.cs
@@ -6,10 +6,10 @@ namespace StarwebSharp.Entities
     {
         [JsonProperty("invoice",
             NullValueHandling = NullValueHandling.Ignore)]
-        public AddressModel Invoice { get; set; } = new AddressModel();
+        public AddressModel Invoice { get; set; }
 
         [JsonProperty("delivery",
             NullValueHandling = NullValueHandling.Ignore)]
-        public AddressModel Delivery { get; set; } = new AddressModel();
+        public AddressModel Delivery { get; set; }
     }
 }
