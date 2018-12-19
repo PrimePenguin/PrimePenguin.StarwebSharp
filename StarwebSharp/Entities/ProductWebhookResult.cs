09b53b56193ab73dc5982060859c949d9cfab978
Ajay Kumar <ajayak731@hotmail.com>
2018-12-19 14:35:32 +0530

    fixed product webhook result

--- StarwebSharp/Entities/ProductWebhookResult.cs
+++ StarwebSharp/Entities/ProductWebhookResult.cs
@@ -6,17 +6,5 @@ namespace StarwebSharp.Entities
     {
         [JsonProperty("productId", NullValueHandling = NullValueHandling.Ignore)]
         public int ProductId { get; set; }
-        [JsonProperty("shippingMethodId", NullValueHandling = NullValueHandling.Ignore)]
-        public int ShippingMethodId { get; set; }
-        [JsonProperty("paymentMethodId", NullValueHandling = NullValueHandling.Ignore)]
-        public int PaymentMethodId { get; set; }
-        [JsonProperty("orderStatusId", NullValueHandling = NullValueHandling.Ignore)]
-        public int OrderStatusId { get; set; }
-        [JsonProperty("orderStatusIdCode", NullValueHandling = NullValueHandling.Ignore)]
-        public object OrderStatusIdCode { get; set; }
-        [JsonProperty("totalAmount", NullValueHandling = NullValueHandling.Ignore)]
-        public string TotalAmount { get; set; }
-        [JsonProperty("currencyCode", NullValueHandling = NullValueHandling.Ignore)]
-        public string CurrencyCode { get; set; }
     }
 }
