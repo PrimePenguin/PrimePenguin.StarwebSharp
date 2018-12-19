33593081225f0b74dcbfedbfb6f2295283cb2721
Ajay Kumar <ajayak731@hotmail.com>
2018-12-19 14:13:14 +0530

    Added webhook result models

---
+++ StarwebSharp/Entities/WebhookResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace StarwebSharp.Entities
+{
+    public class WebhookResult
+    {
+        [JsonProperty("eventId", NullValueHandling = NullValueHandling.Ignore)]
+        public string EventId { get; set; }
+        [JsonProperty("shopIdCode", NullValueHandling = NullValueHandling.Ignore)]
+        public string ShopIdCode { get; set; }
+        [JsonProperty("eventTimeStamp", NullValueHandling = NullValueHandling.Ignore)]
+        public DateTime EventTimeStamp { get; set; }
+    }
+}
