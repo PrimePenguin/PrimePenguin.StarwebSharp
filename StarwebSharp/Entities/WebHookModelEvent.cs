87780a3fcf62a516c77b737a9510179a900ef756
Ajay Kumar <ajayak731@hotmail.com>
2019-02-22 13:43:34 +0530

    Added order-deleted webhook event

--- StarwebSharp/Entities/WebHookModelEvent.cs
+++ StarwebSharp/Entities/WebHookModelEvent.cs
@@ -17,13 +17,14 @@ namespace StarwebSharp.Entities
 
         [EnumMember(Value = "order-updated")] OrderUpdated = 4,
 
-        [EnumMember(Value = "order-status-changed")]
-        OrderStatusChanged = 5,
+        [EnumMember(Value = "order-deleted")] OrderDeleted = 5,
 
-        [EnumMember(Value = "shop-opened")] ShopOpened = 6,
+        [EnumMember(Value = "order-status-changed")] OrderStatusChanged = 6,
 
-        [EnumMember(Value = "shop-closed")] ShopClosed = 7,
+        [EnumMember(Value = "shop-opened")] ShopOpened = 7,
 
-        [EnumMember(Value = "shop-removed")] ShopRemoved = 8
+        [EnumMember(Value = "shop-closed")] ShopClosed = 8,
+
+        [EnumMember(Value = "shop-removed")] ShopRemoved = 9
     }
 }
